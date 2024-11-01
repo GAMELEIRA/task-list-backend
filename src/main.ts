769186642f7a9a0a9d8eae1d6e0090a9a5ea7561
gabriel.gsantos <gabriel.gsantos@totvs.com.br>
2024-11-01 16:40:06 -0300

    feat(feature/post-user): Adiciona estrutura inicial para o cadastro de usuario

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+const configDocumentationApi = async (app: any) => {
+  const config = new DocumentBuilder()
+    .setTitle('Task List API')
+    .setDescription('API REST para gerenciar tarefas')
+    .setVersion('1.0')
+    .addTag('Task List API')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs/api-task-list', app, documentFactory);
+};
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  configDocumentationApi(app);
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
